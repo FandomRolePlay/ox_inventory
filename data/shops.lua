e0ca1cf1d80cff9a615c48470f96cc17c389e7bf
FandomRP <dev@fandomrp.pl>
2025-07-15 23:40:38 +0200

    Dodanie BMX do tricków w hurtowni siłowni

--- data/shops.lua
+++ data/shops.lua
@@ -1691,7 +1691,7 @@ return {
 			{ name = 'junkp',         price = 12 },
 			{ name = 'cruiser2',    price = 100 },
 			{ name = 'bmx3',        price = 200 },
-			{ name = 'bmx4',        price = 500 },
+			{ name = 'bmxp2',        price = 500 },
 			{ name = 'scorcher2',   price = 350 },
 			{ name = 'tribike4',    price = 500 },
 			{ name = 'skateboard', price = 50 },
